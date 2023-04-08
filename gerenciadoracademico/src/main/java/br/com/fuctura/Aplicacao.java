8a4e4e9e8499d63efd6bb2a657d9466df222dfe9
javajdk2023 <javajdk2023@gmail.com>
2023-04-08 18:32:35 -0300

    adicionando codigo para abrir conexao

--- gerenciadoracademico/src/main/java/br/com/fuctura/Aplicacao.java
+++ gerenciadoracademico/src/main/java/br/com/fuctura/Aplicacao.java
@@ -1,7 +1,20 @@
 package br.com.fuctura;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Aplicacao {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		String url = "jdbc:postgresql://localhost:5432/fuctura";
+		Properties props = new Properties();
+		props.setProperty("user", "fuctura");
+		props.setProperty("password", "123");
+		
+		Connection conn = DriverManager.getConnection(url, props);
+		
+		System.out.println("Conectado com sucesso!");
 
 	}
 }
