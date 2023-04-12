fec0cd8f950a7175c5a9cd784f38547793b7aa20
javajdk2023 <javajdk2023@gmail.com>
2023-04-11 21:46:34 -0300

    aula3

--- gerenciadoracademico/src/main/java/br/com/fuctura/Aplicacao.java
+++ gerenciadoracademico/src/main/java/br/com/fuctura/Aplicacao.java
@@ -2,19 +2,60 @@ package br.com.fuctura;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import br.com.fuctura.entidade.Aluno;
+
 public class Aplicacao {
 	public static void main(String[] args) throws SQLException {
-		String url = "jdbc:postgresql://localhost:5432/fuctura";
+		String url = "jdbc:postgresql://localhost:5432/postgres";
 		Properties props = new Properties();
 		props.setProperty("user", "fuctura");
 		props.setProperty("password", "123");
-		
-		Connection conn = DriverManager.getConnection(url, props);
-		
+
+		Connection conexao = DriverManager.getConnection(url, props);
+
 		System.out.println("Conectado com sucesso!");
 
+		//Menu
+		//1 - cadastrar Aluno
+		
+		//scanner 
+		String nome = "";
+		System.out.println("Digite seu nome: ");
+		
+		System.out.println("Digite sua idade: ");
+		int idade = 0;
+		
+		System.out.println("Digite seu email: ");
+		String email = "";
+		
+		//for, while ou do-while
+		Aluno aluno = new Aluno();
+		aluno.setNome(nome);
+		aluno.setIdade(idade);
+		aluno.setEmail(email);
+		
+		inserirAluno(conexao, aluno);
+		
+		
+		System.out.println("Inserido com sucesso!");
 	}
+
+	static void inserirAluno(Connection conexao, Aluno aluno) throws SQLException {
+		String sql = "INSERT INTO aluno VALUES (?, ?, ?)";
+
+		PreparedStatement pstm = conexao.prepareStatement(sql);
+
+		// (indice, valor)
+		pstm.setString(1, aluno.getNome());
+		pstm.setInt(2, aluno.getIdade());
+		pstm.setString(3, aluno.getEmail());
+
+		// executar
+		pstm.execute();
+	}
+
 }
---
+++ gerenciadoracademico/src/main/java/br/com/fuctura/entidade/Aluno.java
@@ -0,0 +1,34 @@
+package br.com.fuctura.entidade;
+
+public class Aluno {
+	private String nome;
+	private int idade;
+	private String email;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	// getter e setter
+
+}
